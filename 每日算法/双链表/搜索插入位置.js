db6f3d331a3ccc11102a50d89b8c95879e4efcf4
hanhan <1551547222@qq.com>
2020-11-26 07:10:04 +0800

    2020-11-26

--- 每日算法/双链表/搜索插入位置.js
+++ 每日算法/双链表/搜索插入位置.js
@@ -11,7 +11,7 @@ var searchInsert = function(nums, target) {
     let l = 0;
     let r = nums.length - 1;
     while(l <= r){
-        let mid = Math.floor((l+r)/2);
+        let mid = (l + r) >> 1;
         if(target == nums[mid]){
             return mid;
         }
